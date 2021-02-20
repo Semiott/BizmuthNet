508db8e089ad90baf5dc69ec62087b20c2fdf78e
Gokul Alex <gokulsan@users.noreply.github.com>
2021-02-20 09:22:30 +0530

    Update Stabilizer.sol

--- contracts/dao/Stabilizer.sol
+++ contracts/dao/Stabilizer.sol
@@ -9,7 +9,7 @@ contract Stabilizer is Setters, Comptroller {
     event StabilityReward(uint256 indexed epoch, uint256 rate, uint256 amount);
 
     function step() internal {
-        Decimal.D256 memory debtRatio = Decimal.ratio(totalDebt(), dollar().totalSupply());
+        Decimal.D256 memory debtRatio = Decimal.ratio(totalDebt(), base().totalSupply());
         debtRatio = debtRatio.greaterThan(Constants.getDebtRatioCap()) ? Constants.getDebtRatioCap() : debtRatio;
 
         Decimal.D256 memory epochRate = Constants.getStabilityRewardMin().add(debtRatio.mul(Constants.getStabilityRewardRate()));
